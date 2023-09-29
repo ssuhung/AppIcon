a3e3f278c6b8029f23f3e823ca84e0746089e725
Ssuhung Yeh <ssuhung@gmail.com>
2023-09-30 08:29:11 +0900

    Extract App list to change icon to a json file

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,63 @@
+extern crate serde;
+extern crate serde_json;
+
 use std::env;
 use std::process::Command;
+use serde_json::Value;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let app_folder = "/Applications";
-    let app_list = vec![
-        "Visual Studio Code",
-        "The Unarchiver",
-        "Microsoft Word",
-        "Microsoft PowerPoint",
-        "Microsoft Excel",
-        "Microsoft OneNote",
-        "zoom.us",
-        "Google Chrome",
-        "AppCleaner",
-        "24 Hour Wallpaper",
-    ];
 
-    if Command::new("fileicon").arg("--version").stdout(std::process::Stdio::null()).status().is_ok() {
-        let script_path = match env::current_exe() {
-            Ok(path) => path.parent().unwrap().to_str().unwrap().to_string(),
-            Err(_) => {
-                eprintln!("Error getting script path");
-                return;
-            }
-        };
-
-        for app_name in &app_list {
-            let app_path = format!("{app_folder}/{app_name}.app");
-            let icon_path = format!("{script_path}/../icon/{app_name}.icns");
-
-            let status = Command::new("fileicon")
-                .arg("set")
-                .arg(&app_path)
-                .arg(&icon_path)
-                .status();
-
-            match status {
-                Ok(_) => (),
-                Err(_) => eprintln!("Error setting icon for {}", app_name),
-            }
+    let project_root = match env::current_dir() {
+        Ok(path) => path.to_str().unwrap().to_string(),
+        Err(_) => {
+            eprintln!("Error getting script path");
+            return;
         }
-    } else {
+    };
+    
+    if Command::new("fileicon").arg("--version").stdout(std::process::Stdio::null()).status().is_err() {
         eprintln!("fileicon is not installed, checkout https://github.com/mklement0/fileicon to install it");
+        return;
+    }    
+    
+    let json_path = format!("{project_root}/icon/app_list.json");
+    let mut file = File::open(json_path).expect("Failed to open file");
+    let mut json_data = String::new();
+    file.read_to_string(&mut json_data).expect("Failed to read file");
+
+    // Parse the JSON data into a serde_json::Value
+    let json_value: Value = serde_json::from_str(&json_data).expect("Failed to parse JSON");
+
+    // Assuming your JSON contains an array of strings, extract it into a vector
+    let mut app_list: Vec<String> = Vec::new();
+    if let Value::Array(string_array) = json_value {
+        app_list = string_array
+            .iter()
+            .filter_map(|item| item.as_str().map(|s| s.to_owned()))
+            .collect();
+
+        // Now you have your vector of strings
+        // println!("{:?}", string_vector);
+    } else {
+        println!("JSON does not contain an array of strings");
+    }
+
+    for app_name in &app_list {
+        let app_path = format!("{app_folder}/{app_name}.app");
+        let icon_path = format!("{project_root}/icon/{app_name}.icns");
+
+        let status = Command::new("fileicon")
+            .arg("set")
+            .arg(&app_path)
+            .arg(&icon_path)
+            .status();
+
+        match status {
+            Ok(_) => (),
+            Err(_) => eprintln!("Error setting icon for {}", app_name),
+        }
     }
 }
