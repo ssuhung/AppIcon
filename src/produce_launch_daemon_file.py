011715312abb46f901288122993611bb959d663e
Ssuhung Yeh <ssuhung@gmail.com>
2024-03-11 04:57:52 +0900

    Change app path to dynamic when generate service file

--- src/produce_launch_daemon_file.py
+++ src/produce_launch_daemon_file.py
@@ -2,11 +2,13 @@ import json
 import os
 import xml.etree.ElementTree as ET
 
+
 XML_DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>\n'
 DOCTYPE_DECLARATION = '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n'
 APPLICATION_DIR = '/Applications/'
 app_list_dir = '../icon/app_list.json'
 launch_daemon_file = 'com.ssuhung.appUpdateDetector.plist'
+app_path = os.path.dirname(os.path.dirname(__file__)) + '/target/debug/app_icon'
 
 with open(app_list_dir, "r") as json_file:
     app_list = json.load(json_file)
@@ -15,6 +17,17 @@ watch_paths_list = [os.path.join(APPLICATION_DIR, app_name + '.app') for app_nam
 tree = ET.parse('./service_template.plist')
 root = tree.getroot()
 dict_ele = root.find('dict')
+
+# Insert key ProgramArguments
+key_ele = ET.Element('key')
+key_ele.text = 'ProgramArguments'
+dict_ele.append(key_ele)
+
+# Insert ProgramArguments Array
+paths_ele = ET.Element('array')
+ele = ET.SubElement(paths_ele, 'string')
+ele.text = app_path
+dict_ele.append(paths_ele)
 
 # Insert key WatchPaths
 key_ele = ET.Element('key')
